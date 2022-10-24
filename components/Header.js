56a6ba896a6b4d43d8aa3ef05c7aa7ff40d2934a
Yunus Eş <yunuses@newmind.ai>
2022-10-24 23:14:49 +0300

    Styled denemeleri yapıldı

--- components/Header.js
+++ components/Header.js
@@ -2,20 +2,39 @@ import Link from "next/link";
 import styled from "styled-components";
 
 const HeaderDiv = styled.div`
-  color: #f00;
   border: 10px solid #f00;
   padding: 30px;
   background-color: #f00;
+  display: flex;
+  gap: 20px;
+
+  & > span {
+    font-size: 30px;
+  }
+`;
+
+const StyledA = styled.a`
+  color: #fff;
+  text-decoration: none;
+  border: ${({ borderSize }) => `${borderSize}px`} solid yellow;
+  font-size: ${({ big }) => (big ? "20px" : "12px")};
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default () => (
   <HeaderDiv>
     <Link href="/" passHref>
-      <a>Ana Sayfa</a>
+      <StyledA borderSize={20}>Ana Sayfa</StyledA>
     </Link>
 
     <Link href="/about" passHref>
-      <a>Hakkımızda</a>
+      <StyledA borderSize={10} big>
+        Hakkımızda
+      </StyledA>
     </Link>
+
+    <span>Ek açıklamalar</span>
   </HeaderDiv>
 );
--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import { createGlobalStyle } from "styled-components";
 const GlobalStyle = createGlobalStyle`
   body {
     margin: 0;
+    font-family: sans-serif;
   }
 `;
 
