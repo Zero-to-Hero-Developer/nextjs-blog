04cbb7cf65a2fabb11a0c7809e122c101478ce41
Yunus Eş <yunuses@newmind.ai>
2022-10-31 22:45:01 +0300

    MD dosyalarıyla çalışır hale getirildi

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,48 +1,58 @@
+import fs from "fs";
+import matter from "gray-matter";
+import md from "markdown-it";
 import { useRouter } from "next/router";
 import Error from "next/error";
 import Layout from "../../components/Layout";
 
-const data = [
-  {
-    slug: "gunluk-1",
-    title: "Günlük 1",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-  Lorem Ipsum has been the industry's standard dummy text ever since the
-  1500s, when an unknown printer took a galley of type and scrambled it to
-  make a type specimen book. It has survived not only five centuries, but also
-  the leap into electronic typesetting, remaining essentially unchanged. It
-  was popularised in the 1960s with the release of Letraset sheets containing
-  Lorem Ipsum passages, and more recently with desktop publishing software
-  like Aldus PageMaker including versions of Lorem Ipsum.`,
-  },
-  {
-    slug: "gunluk-2",
-    title: "Günlük 2",
-    text: `Günlük yazı içeriği 2. Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-  Lorem Ipsum has been the industry's standard dummy text ever since the
-  1500s, when an unknown printer took a galley of type and scrambled it to
-  make a type specimen book. It has survived not only five centuries, but also
-  the leap into electronic typesetting, remaining essentially unchanged. It
-  was popularised in the 1960s with the release of Letraset sheets containing
-  Lorem Ipsum passages, and more recently with desktop publishing software
-  like Aldus PageMaker including versions of Lorem Ipsum.`,
-  },
-];
+export default ({ frontmatter, content }) => {
+  const { title, author, category, date, bannerImage, tags } = frontmatter;
 
-export default ({}) => {
-  const router = useRouter();
-  const { slug } = router.query;
+  // const router = useRouter();
+  // const { slug } = router.query;
 
-  let foundItem = data.find((dataItem) => dataItem.slug === slug);
+  // let foundItem = data.find((dataItem) => dataItem.slug === slug);
 
-  if (!foundItem) {
-    return <Error statusCode={404} />;
-  }
+  // if (!foundItem) {
+  //   return <Error statusCode={404} />;
+  // }
 
   return (
-    <Layout title={foundItem?.title}>
-      <h1>{slug}</h1>
-      <p>{foundItem?.text}</p>
+    <Layout title={title}>
+      <h2>
+        {author} || {date}
+      </h2>
+      <h3>
+        {category} || {tags.join()}
+      </h3>
+      <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
     </Layout>
   );
 };
+
+export async function getStaticPaths() {
+  // Get list of all files from our posts directory
+  const files = fs.readdirSync("posts");
+  // Generate a path for each one
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(".md", ""),
+    },
+  }));
+  // return list of paths
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const fileName = fs.readFileSync(`posts/${slug}.md`, "utf-8");
+  const { data: frontmatter, content } = matter(fileName);
+  return {
+    props: {
+      frontmatter,
+      content,
+    },
+  };
+}
---
+++ posts/konu2.md
@@ -0,0 +1,13 @@
+---
+title: "This is an example post 2"
+author: "Yunus Eş"
+category: "example"
+date: "2022-03-13"
+bannerImage: "url-to-image.png"
+tags:
+  - example
+---
+
+## This is an example blog post 2
+
+This is sample content. The section above is called Frontmatter where we can add post metadata like title and author. You can add as little or as many properties in the frontmatter using YAML syntax.
